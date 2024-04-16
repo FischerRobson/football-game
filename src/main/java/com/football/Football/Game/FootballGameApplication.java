30db5ffb3cb71564796bd202f71eb2fb428751a1
Robson Fischer <fischerrobson@gmail.com>
2024-04-15 23:41:27 -0300

    hidden secrets

--- src/main/java/com/football/Football/Game/FootballGameApplication.java
+++ src/main/java/com/football/Football/Game/FootballGameApplication.java
@@ -1,5 +1,6 @@
 package com.football.Football.Game;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FootballGameApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
 		SpringApplication.run(FootballGameApplication.class, args);
 	}
 
